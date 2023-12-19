768705055c50699d89d7cd036b0912540807dcf2
Liispet-Tess Talu <liispet-tesstalu@LiispetTesssMBP.lan>
2023-12-19 18:13:43 +0200

    create new app

---
+++ Week13SQLite/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SQLite;
+
+ReadData(CreateConnection());
+
+
+static SQLiteConnection CreateConnection()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version = 3; New = True; Compress = True");
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("DB found.");
+    }
+    catch
+    {
+        Console.WriteLine("DB not found");
+    }
+    return connection;
+}
+
+static void ReadData(SQLiteConnection myConnection)
+{
+    Console.Clear();
+    SQLiteDataReader reader;
+    SQLiteCommand command;
+
+    command = myConnection.CreateCommand();
+    command.CommandText = "SELECT * FROM customer";
+
+    reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string readerStringFirstName = reader.GetString(0);
+        string readerStringLastName = reader.GetString(1);
+        string readerStringDoB = reader.GetString(2);
+
+        Console.WriteLine($"Full name: {readerStringFirstName} {readerStringLastName} DoB: {readerStringDoB}");
+    }
+
+    myConnection.Close();
+}
